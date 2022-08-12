2ad6916e34ad3f63347ce7eaa5dbe5b6617dbefb
Sloane Tribble <SloaneTribble@gmail.com>
2022-08-12 15:03:25 -0700

    Implement gameboard and knight functions

--- dist/bundle.js
+++ dist/bundle.js
@@ -3,6 +3,37 @@ var __webpack_exports__ = {};
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
+const gameBoard = function generateBoardCoords() {
+  let coords = [];
+
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      coords.push([i, j]);
+    }
+  }
+
+  return coords;
+};
+
+// Attempt to treat knight as a node which stores its current location
+
+const knight = function knightNode(coord = null) {
+  const knight = {};
+
+  knight.coord = coord;
+
+  // Maximum eight possible moves for any given coordinate
+
+  knight.tl1 = null;
+  knight.tl2 = null;
+  knight.tr1 = null;
+  knight.tr2 = null;
+  knight.br1 = null;
+  knight.br2 = null;
+  knight.bl1 = null;
+  knight.bl2 = null;
+};
 
 /******/ })()
 ;
+//# sourceMappingURL=bundle.js.map
